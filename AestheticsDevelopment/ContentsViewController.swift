00e626ea9a47bb8bc821c6c524144e827f77f987
sugiy118 <sugiyamayu118@gmail.com>
2016-07-19 16:46:43 +0900

    first commit

--- AestheticsDevelopment/ContentsViewController.swift
+++ AestheticsDevelopment/ContentsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ContentsViewController.swift
 //  AestheticsDevelopment
 //
 //  Created by YU SUGIYAMA on 2016/07/19.
@@ -8,18 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ContentsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
 }
-
---
+++ AestheticsDevelopment/ProfileTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileTableViewCell.swift
+//  AestheticsDevelopment
+//
+//  Created by YU SUGIYAMA on 2016/07/19.
+//  Copyright © 2016年 Yu Sugiyama. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
