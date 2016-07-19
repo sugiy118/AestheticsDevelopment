a21938782363190828aac287b92f735277ed3cda
sugiy118 <sugiyamayu118@gmail.com>
2016-07-19 13:28:06 +0900

    Initial Commit

---
+++ AestheticsDevelopment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AestheticsDevelopment
+//
+//  Created by YU SUGIYAMA on 2016/07/19.
+//  Copyright © 2016年 Yu Sugiyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
