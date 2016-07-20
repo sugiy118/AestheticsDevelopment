6f97723687e0adf60e54511a5b6ecd73a7ea80d1
sugiy118 <sugiyamayu118@gmail.com>
2016-07-20 18:14:40 +0900

    commit xib embedding

--- AestheticsDevelopment/ContentsTableView.swift
+++ AestheticsDevelopment/ContentsTableView.swift
@@ -8,16 +8,42 @@
 
 import UIKit
 
-class ContentsTableView: UITableView {
+class ContentsTableView: UITableView, UITableViewDelegate, UITableViewDataSource {
 
     override init(frame: CGRect, style: UITableViewStyle) {
         super.init(frame: frame, style: style)
+        
+        self.delegate = self
+        self.dataSource = self
+        
+        self.registerNib(UINib(nibName: "ContentsTopTableViewCell", bundle: nil), forCellReuseIdentifier: "ContentsTopTableViewCell")
     }
+    
+    let blue = UIColor(red: 92.0 / 255, green: 192.0 / 255, blue: 210.0 / 255, alpha: 1.0)
     
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    //セル数
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    //セル内容
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ContentsTopTableViewCell", forIndexPath: indexPath) as! ContentsTopTableViewCell
+        cell.locationImageView.image = UIImage(named: "tokyo_top_image")
+        cell.locationName.text = "TOKYO"
+        cell.imageMaskView.backgroundColor = blue
+        return cell
+    }
+    
+    //セルの高さ
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 200
+    }
+}
     
     
     /*
@@ -28,4 +54,4 @@ class ContentsTableView: UITableView {
     }
     */
 
-}
+
